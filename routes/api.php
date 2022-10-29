baaef5523eb268068e39389cfc1106da7e28b027
a.malkawi <abdallahmalkawi0@gmail.com>
2022-10-29 13:48:11 +0300

    Fix API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'] , function () {
+    Route::post('category/store' , [CategoryController::class , 'store'])->name('category.store');
+    Route::get('categories' , [CategoryController::class , 'index'])->name('category.index');
+
+    Route::post('transaction/store' , [TransactionController::class , 'store'])->name('transaction.store');
+    Route::get('transactions' , [TransactionController::class , 'index'])->name('transaction.index');
+});
+
+Route::group(['middleware' => 'validation'] , function () {
+    Route::post('register' , [UserController::class , 'register'])->name('register');
+    Route::post('login' , [UserController::class , 'login'])->name('login')->middleware('login.auth');
 });
